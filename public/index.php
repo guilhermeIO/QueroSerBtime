2db5fb4d79eb369d771abed8cd78b7a49ea6c842
Guilherme Maia <guilhermecmaia.io@gmail.com>
2018-08-31 05:47:52 -0300

    Aplicada simulação de cenário no arquivo de entrada do projeto.

--- public/index.php
+++ public/index.php
@@ -1,3 +1,21 @@
 <?php
 
-require_once __DIR__ . '../bootstrap/bootstrap.php';
+require_once __DIR__ . '/../bootstrap/bootstrap.php';
+
+use App\Cursor;
+use App\MagneticPoint;
+
+try {
+    $cursor = new Cursor();
+
+    $magneticPoints = [
+        new MagneticPoint(50, 50, 5),
+        new MagneticPoint(51, 51, 5),
+    ];
+
+    $coordinates = $cursor->withMagneticPoints($magneticPoints)->move(51, 52);
+
+    echo "X: {$coordinates[0]}, Y: {$coordinates[1]}" . PHP_EOL;
+} catch (Exception $e) {
+    echo $e->getMessage() . PHP_EOL;
+}
